5cfd5951cf60e456368206ea5bb649ff019ad6eb
Massimo <massimo.biancalani@gmail.com>
2022-08-19 13:09:05 +0200

    SuperAdder

---
+++ src/main/scala/ch02_monoids/SuperAdder.scala
@@ -0,0 +1,27 @@
+package ch02_monoids
+
+import cats.Monoid
+
+object SuperAdder {
+  def add(items: List[Int]): Int = {
+    val monoid = Monoid[Int]
+    items.foldRight(monoid.empty)(monoid.combine)
+  }
+
+  def add2[A](items: List[A])(implicit monoid: Monoid[A]): A = {
+    items.foldRight(monoid.empty)(monoid.combine)
+  }
+}
+
+case class Order(totalCost: Double, quantity: Double)
+
+object Order {
+  import cats.syntax.semigroup._
+  import cats.instances.double._
+
+  implicit val orderMonoid: Monoid[Order] = new Monoid[Order] {
+    override def empty: Order = Order(0.0, 0.0)
+    override def combine(x: Order, y: Order): Order =
+      Order(x.totalCost |+| y.totalCost, x.quantity |+| y.quantity)
+  }
+}
---
+++ src/test/scala/ch02_monoids/SuperAdderTest.scala
@@ -0,0 +1,33 @@
+package ch02_monoids
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class SuperAdderTest extends AnyFlatSpec with Matchers {
+
+  behavior of "add"
+
+  it should "sum the content of a list" in {
+    SuperAdder.add(List(1, 2, 3)) should be (6)
+  }
+
+
+  behavior of "add2"
+
+  it should "sum the content of a list of int" in {
+    SuperAdder.add2(List(1, 2, 3)) should be (6)
+  }
+
+  it should "sum the content of a list of option of int" in {
+    val in: List[Option[Int]] = List(Some(1), Some(2), Some(3))
+    SuperAdder.add2(in) should be (Some(6))
+  }
+
+
+  behavior of "order"
+
+  it should "be able to be summed in add2" in {
+    val in = List(Order(10.0, 2.0), Order(2.0, 1.5))
+    SuperAdder.add2(in) should be (Order(12.0, 3.5))
+  }
+}
